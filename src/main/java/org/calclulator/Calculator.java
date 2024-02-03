ee056169be3bf13b809e52a9720e69ad80fa9fc0
Iltekov Vladislav <vlad.236@yandex.ru>
2024-02-03 23:51:42 +0800

    update_home_03.02.24

--- src/main/java/org/calclulator/Calculator.java
+++ src/main/java/org/calclulator/Calculator.java
@@ -1,12 +1,12 @@
 package org.calclulator;
 
+import lombok.Getter;
+
 public class Calculator {
+
+    @Getter
     private double calories;
     private void calculator(double foodCalories, double foodWeight){
         calories = (foodCalories/100)*foodWeight;
-    }
-
-    public double getCalories(){
-        return calories;
     }
 }
